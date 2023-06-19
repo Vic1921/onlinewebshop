5f5f907f8294b71db13cb79a93a7aff28d7cf1ef
Victor Binzar <a12022144@unet.univie.ac.at>
2023-06-19 19:28:04 +0200

    Implemented OrderService and OrderFulfillmentService to cover our main use-cases. Some fixes are needed

---
+++ src/main/java/at/dke/onlinewebshop/sql/services/OrderFulfillmentService.java
@@ -0,0 +1,48 @@
+package at.dke.onlinewebshop.sql.services;
+import at.dke.onlinewebshop.sql.entities.Article;
+import at.dke.onlinewebshop.sql.entities.Order;
+import at.dke.onlinewebshop.sql.entities.Warehouse;
+import at.dke.onlinewebshop.sql.repositories.OrderRepository;
+import at.dke.onlinewebshop.sql.repositories.WarehouseRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OrderFulfillmentService {
+
+    private final OrderRepository orderRepository;
+    private final WarehouseRepository warehouseRepository;
+
+    public OrderFulfillmentService(OrderRepository orderRepository, WarehouseRepository warehouseRepository) {
+        this.orderRepository = orderRepository;
+        this.warehouseRepository = warehouseRepository;
+    }
+
+    @Transactional
+    public Order fulfillOrder(Long orderId, Long warehouseId) {
+        // Check if the order exists
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new RuntimeException("Order not found"));
+
+        // Check if the warehouse exists
+        Warehouse warehouse = warehouseRepository.findById(warehouseId).orElseThrow(() -> new RuntimeException("Warehouse not found"));
+
+        // Check if all articles in the order are available in the warehouse
+        for (OrderLine orderLine : order.getOrderLines()) {
+            Article article = orderLine.getArticle();
+            int quantity = orderLine.getQuantity();
+
+            // Check if the warehouse has enough stock of the article
+            if (!warehouse.hasEnoughStock(article, quantity)) {
+                throw new RuntimeException("Not enough stock in warehouse for article " + article.getId());
+            }
+
+            // Update the stock in the warehouse
+            warehouse.reduceStock(article, quantity);
+            warehouseRepository.save(warehouse);
+        }
+
+        // Mark the order as fulfilled
+        order.setFulfilled(true);
+        return orderRepository.save(order);
+    }
+}
---
+++ src/main/java/at/dke/onlinewebshop/sql/services/OrderService.java
@@ -0,0 +1,62 @@
+package at.dke.onlinewebshop.sql.services;
+import at.dke.onlinewebshop.sql.entities.Article;
+import at.dke.onlinewebshop.sql.entities.Customer;
+import at.dke.onlinewebshop.sql.entities.Order;
+import at.dke.onlinewebshop.sql.repositories.ArticleRepository;
+import at.dke.onlinewebshop.sql.repositories.CustomerRepository;
+import at.dke.onlinewebshop.sql.repositories.OrderRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final ArticleRepository articleRepository;
+    private final CustomerRepository customerRepository;
+
+    public OrderService(OrderRepository orderRepository, ArticleRepository articleRepository, CustomerRepository customerRepository) {
+        this.orderRepository = orderRepository;
+        this.articleRepository = articleRepository;
+        this.customerRepository = customerRepository;
+    }
+
+    @Transactional
+    public Order placeOrder(Long customerId, Map<Long, Integer> articles) {
+        // Check if the customer exists
+        Customer customer = customerRepository.findById(customerId).orElseThrow(() -> new RuntimeException("Customer not found"));
+
+        // Create a new order for the customer
+        Order order = new Order();
+        order.setCustomer(customer);
+
+        // For each article in the order, check if it exists and if there is enough stock
+        for (Map.Entry<Long, Integer> entry : articles.entrySet()) {
+            Article article = articleRepository.findById(entry.getKey()).orElseThrow(() -> new RuntimeException("Article not found"));
+            int quantity = entry.getValue();
+
+            // Check if there is enough stock
+            if (article.getStock() < quantity) {
+                throw new RuntimeException("Not enough stock for article " + article.getId());
+            }
+
+            // Create a new order line for the article
+            OrderLine orderLine = new OrderLine();
+            orderLine.setArticle(article);
+            orderLine.setQuantity(quantity);
+            orderLine.setOrder(order);
+
+            // Add the order line to the order
+            order.getOrderLines().add(orderLine);
+
+            // Update the stock of the article
+            article.setStock(article.getStock() - quantity);
+            articleRepository.save(article);
+        }
+
+        // Save the order
+        return orderRepository.save(order);
+    }
+}
